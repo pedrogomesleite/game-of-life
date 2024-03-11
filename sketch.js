91144d94b2be1f2d9bfe5175265203d72841f861
kracatua <pedro.gomes@funcionarios.automatizai.com.br>
2024-03-11 18:19:30 -0300

    Final changes

--- sketch.js
+++ sketch.js
@@ -12,22 +12,49 @@ function setup() {
   for (let i = 0; i < gridW; i++) {
     grid[i] = [];
     for (let j = 0; j < gridH; j++) {
-      grid[i][j] = 0;
+      grid[i][j] = round(random(0,1));
     }
   }
 }
 
 function draw() {
   background(220);
-
+  let newGrid = [];
   for (let i = 0; i < gridW; i++) {
+    newGrid[i] = [];
     for (let j = 0; j < gridH; j++) {
-      verificarMorte(grid, i, j);
-      verificarVida(grid, i, j);
-      aleatorizarVida(grid, i, j);
+      let nei = getNei(grid, i, j);
+      if(grid[i][j] == 1 && (nei < 2 || nei > 3)) {
+        newGrid[i][j] = 0;   
+      }
+      else if(grid[i][j] == 0 && (nei == 3)){
+        newGrid[i][j] = 1;
+      }
+      else {
+        newGrid[i][j] = grid[i][j];
+      }
+      
+      
+      
+      // aleatorizarVida(grid, i, j, newGrid);
       gerar(grid, i, j);
     }
   }
+  grid = newGrid;
+}
+
+function getNei(grid, h, w) {
+  let count = 0;
+  for (let i = max(h - 1, 0); i <= min(h + 1, gridH - 1); i++) {
+    for (let j = max(w - 1, 0); j <= min(w + 1, gridW - 1); j++) {
+      if (typeof grid[i] !== 'undefined' && typeof grid[i][j] !== 'undefined') {
+        if (grid[i][j] == 1) {
+          count++;
+        }
+      }
+    }
+  }
+  return count - grid[h][w];
 }
 
 function gerar(grid, h, w) {
@@ -41,48 +68,12 @@ function gerar(grid, h, w) {
   }
 }
 
-function aleatorizarVida(grid, h, w) {
+function aleatorizarVida(grid, h, w, newGrid) {
   if (grid[h][w] == 1) {
     let chance = round(random(0, 4));
     if (chance < 1) {
-      grid[h][w] = 0;
+      newGrid[h][w] = 0;
     }
-  }
-}
-
-function verificarVida(grid, h, w) {
-  let count = 0;
-  if (grid[h][w] == 0) {
-    for (let i = max(h - 1, 0); i <= min(h + 1, gridH - 1); i++) {
-      for (let j = max(w - 1, 0); j <= min(w + 1, gridW - 1); j++) {
-        if (typeof grid[i] !== 'undefined' && typeof grid[i][j] !== 'undefined') {
-          if (grid[i][j] == 1) {
-            count++;
-          }
-        }
-      }
-    }
-  }
-  if (count == 3) {
-    grid[h][w] = 1;
-  }
-}
-
-function verificarMorte(grid, h, w) {
-  let count = 0;
-  if (grid[h][w] == 1) {
-    for (let i = max(h - 1, 0); i <= min(h + 1, gridH - 1); i++) {
-      for (let j = max(w - 1, 0); j <= min(w + 1, gridW - 1); j++) {
-        if (typeof grid[i] !== 'undefined' && typeof grid[i][j] !== 'undefined') {
-          if (grid[i][j] == 1) {
-            count++;
-          }
-        }
-      }
-    }
-  }
-  if (count < 2 || count > 3) {
-    grid[h][w] = 0;
   }
 }
 
